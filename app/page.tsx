63a2b9389921bbe4a2f947efe7ee24c93ae2d741
minhyun <122688264+m2nhyun@users.noreply.github.com>
2026-07-24 15:24:27 +0900

    Merge pull request #1 from m2nhyun/assets-for-code
    refactor: assets, codes for next

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import { ContactSection } from "@/components/contact-section";
 import { EventSection } from "@/components/event-section";
 import { GallerySection } from "@/components/gallery-section";
 import { IntroSection } from "@/components/intro-section";
+import { ScrollMotion } from "@/components/scroll-motion";
 
 export default function Page() {
   return (
     <main>
+      <ScrollMotion />
       <IntroSection />
       <EventSection />
       <ContactSection />
