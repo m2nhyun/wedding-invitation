9e03c66f86dd2a6f6ef34a74da35801f938f60a1
onpagethree <140528669+onpagethree@users.noreply.github.com>
2026-08-13 01:16:53 +0900

    Restore standalone event details slide

--- src/components/event-section.tsx
+++ src/components/event-section.tsx
@@ -3,14 +3,34 @@ import { wedding } from "@/data/wedding";
 
 export function EventSection() {
 	return (
-		<div className="event-photo">
-			<Image
-				src={wedding.assets.eventBackground}
-				alt=""
-				width={2048}
-				height={2560}
-				priority
-			/>
-		</div>
+		<section className="event-slide">
+			<div className="event-photo">
+				<Image
+					src={wedding.assets.eventBackground}
+					alt=""
+					width={2048}
+					height={2560}
+					priority
+				/>
+			</div>
+			<div className="event-details">
+				<h2 className="event-heading">
+					{wedding.event.date}
+					<br />
+					{wedding.event.time}
+					<br />
+					{`${wedding.event.venue.replace(", ", "(")})`}
+				</h2>
+				<div className="event-map-links">
+					<a className="map-link" href={wedding.event.naverMapUrl}>
+						{wedding.event.naverMapLabel}
+					</a>
+					<span aria-hidden="true">/</span>
+					<a className="map-link" href={wedding.event.kakaoMapUrl}>
+						{wedding.event.kakaoMapLabel}
+					</a>
+				</div>
+			</div>
+		</section>
 	);
 }
