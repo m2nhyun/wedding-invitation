4f7dd6f6c002ab06e2e806aa57983d84d994851d
onpagethree <140528669+onpagethree@users.noreply.github.com>
2026-08-13 00:53:52 +0900

    Move event details below centered photo

--- src/components/event-section.tsx
+++ src/components/event-section.tsx
@@ -22,27 +22,30 @@ export function EventSection() {
 
 	return (
 		<section className="event-slide">
-			<h2 className="event-heading event-date">
-				{wedding.event.date}
-				<br />
-				{wedding.event.time}
-			</h2>
 			<div
 				className="event-photo"
 				style={{ backgroundImage: `url("${wedding.assets.eventBackground}")` }} />
 			<div className="event-details">
+				<h2 className="event-heading event-date">
+					{wedding.event.date}
+					<br />
+					{wedding.event.time}
+				</h2>
 				<h2 className="event-heading event-location">
 					{`${wedding.event.venue.replace(", ", "(")})`}
 				</h2>
-				<a
-					className="map-link"
-					href={wedding.event.naverMapUrl}
-					onClick={openNaverMap}>
-					{wedding.event.naverMapLabel}
-				</a>
-				<a className="map-link" href={wedding.event.kakaoMapUrl}>
-					{wedding.event.kakaoMapLabel}
-				</a>
+				<div className="event-map-links">
+					<a
+						className="map-link"
+						href={wedding.event.naverMapUrl}
+						onClick={openNaverMap}>
+						{wedding.event.naverMapLabel}
+					</a>
+					<span aria-hidden="true">/</span>
+					<a className="map-link" href={wedding.event.kakaoMapUrl}>
+						{wedding.event.kakaoMapLabel}
+					</a>
+				</div>
 			</div>
 
 			<p className="parking-note">
